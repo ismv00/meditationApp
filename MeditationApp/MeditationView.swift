90f954ac068595e475e2f06cb71ccee0b9b7d0fb
ismv00 <ismv00@icloud.com>
2023-02-14 23:37:54 -0400

    feat:create a main screen

--- MeditationApp/MeditationView.swift
+++ MeditationApp/MeditationView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct MeditationView: View {
     var body: some View {
-        VStack {
-            //MARK: Image
+        VStack(spacing: 0) {
+            // MARK: IMAGE
             Image("image-stones")
                 .resizable()
-                .scaledToFit()
+                .scaledToFill()
                 .frame(height: UIScreen.main.bounds.height / 3)
+            
+            //MARK: Meditation Details
+            ZStack {
+                Color(red: 24/255, green: 23/255, blue: 22/255)
+                VStack(alignment: .leading, spacing: 24) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Music")
+                        Text("0s")
+                    }
+                    .font(.subheadline)
+                    .textCase(.uppercase)
+                    .opacity(0.7)
+                    
+                    Text("1 minuto de meditação")
+                    Text("Limpe sua mente e concetresse no som")
+                    
+                    Spacer()
+                }
+            }
+            .foregroundColor(.white)
+            .frame(height: UIScreen.main.bounds.height * 2 / 3)
         }
+        .ignoresSafeArea()
     }
 }
 
