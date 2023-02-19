ce7ff8be3da7f3f85767fa6f68171592925b20d0
ismv00 <ismv00@icloud.com>
2023-02-19 12:31:07 -0400

    feat:configure audioplayer

--- MeditationApp/AudioManager.swift
+++ MeditationApp/AudioManager.swift
@@ -12,15 +12,21 @@ final class AudioManager {
     static let shared = AudioManager()
     var player: AVAudioPlayer?
     
-    func startPlayer(track: String) {
+    func startPlayer(track: String, isPreview: Bool = false) {
         guard let url = Bundle.main.url(forResource: track, withExtension: "mp3") else {
             print("Resource not found: \(track)")
             return
         }
         do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
             player = try AVAudioPlayer(contentsOf: url)
+            if isPreview {
+                player?.prepareToPlay()
+            }else {
+                player?.play()
+            }
             
-            player?.play()
         } catch {
             print("Fail to initialize player", error)
         }
